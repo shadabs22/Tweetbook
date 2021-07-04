b3ed9db928bd9c21b008868d947e3166ad947f6a
Mohd Shadab Shaikh <shadabs22@gmail.com>
2021-07-04 17:47:26 +0530

    tutorial 4

--- Tweetbook/Controllers/v1/PostsController.cs
+++ Tweetbook/Controllers/v1/PostsController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tweetbook.Contracts.v1;
+using Tweetbook.Contracts.v1.Requests;
+using Tweetbook.Contracts.v1.Responses;
 using Tweetbook.Domain.v1;
 
 namespace Tweetbook.Controllers.v1
@@ -25,6 +27,21 @@ namespace Tweetbook.Controllers.v1
         public async Task<IActionResult> GetAll()
         {
             return Ok(posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public async Task<IActionResult> Create([FromBody] CreatePostRequest createPost)
+        {
+            var post = new Post() { id = createPost.id };
+            if (post == null || string.IsNullOrEmpty(post.id))
+            {
+                post = new Post() { id = "" + Guid.NewGuid() };
+            }
+            posts.Add(post);
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.id);
+            var postResponse = new PostResponse() { id = post.id };
+            return Created(locationUri, postResponse);
         }
     }
 }
